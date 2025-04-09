209919d1e823d177b962577caee3ebfbc0df2de1
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-09 22:58:04 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div id="app" className="flex h-screen">
+			<div className="editor w-2/6 p-12 bg-slate-50 shadow-2xl">
+				<h1 className="logo text-3xl font-bold">ResumePro</h1>
+			</div>
+			<div className="review w-4/6 p-12">hi</div>
+		</div>
+	);
+}
+
+export default App;
